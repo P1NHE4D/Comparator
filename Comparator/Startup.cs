a498c89e20af77b7fbfee3995cfd75258a2a5eb9
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-04-30 20:47:53 +0200

    feat: add logger (COM-5 #resolve)

--- Comparator/Startup.cs
+++ Comparator/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Comparator.Utils.Extensions;
+using Comparator.Utils.Logger;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -25,16 +27,19 @@ namespace Comparator
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.ConfigureLoggerService();
             services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerManager logger)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            logger.LogDebug("Blablabla");
+            logger.LogInfo("Blablabla");
 
             app.UseHttpsRedirection();
 
---
+++ Comparator/Utils/Logger/ILoggerManager.cs
@@ -0,0 +1,10 @@
+namespace Comparator.Utils.Logger
+{
+    public interface ILoggerManager
+    {
+        void LogDebug(string message);
+        void LogError(string message);
+        void LogInfo(string message);
+        void LogWarning(string message);
+    }
+}
