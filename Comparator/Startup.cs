b0454e0159075c661cf27325f7395cf8415e70d0
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-04-30 21:03:17 +0200

    refactor: remove logger from configure method

--- Comparator/Startup.cs
+++ Comparator/Startup.cs
@@ -32,14 +32,12 @@ namespace Comparator
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerManager logger)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
-            logger.LogDebug("Blablabla");
-            logger.LogInfo("Blablabla");
 
             app.UseHttpsRedirection();
 
