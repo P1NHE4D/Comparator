c5d2816fd54f6cb0b423f2151b288b88c1590fd1
Marcel Robohm <internetzverbindung@gmail.com>
2020-05-01 21:17:02 +0200

    fix: added .csproj file

---
+++ Comparator/Utils/Monads/Beispiel.cs
@@ -0,0 +1,54 @@
+using System;
+
+namespace Comparator.Utils.Monads
+{
+    
+    
+    // Unsers
+    public static class FileUser
+    {
+        public static int OpenFile(string filename) =>
+            FileIOAbstract
+                .OpenFile(filename)
+                .Map(x => x * 2)
+                .Map(x => x + 2)
+                .Bind(x => Division(2, x))
+                .Return(0);
+
+        public static Capsule<int> Division(int a, int b)
+        {
+            if (b == 0)
+                return new Failure<int>("Division by zero, du Lappen!!!");
+            return new Success<int>(a / b);
+        }
+    }
+    
+    // Unsers
+    public static class FileIOAbstract
+    {
+        public static Capsule<int> OpenFile(string filename)
+        {
+            try
+            {
+                var result = FileIO.OpenFile(filename);
+                return new Success<int>(result);
+            }
+            catch (Exception e)
+            {
+                return new Failure<int>(e.Message);
+            }
+        }
+    }
+
+    
+    // Fremd
+    static class FileIO
+    {
+        public static int  OpenFile(string filename)
+        {
+            if (filename == "fehler")
+                throw new Exception("Fehler!");
+            return 0;
+        }
+    }
+}
