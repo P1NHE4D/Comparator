9b1dfddb8ae3ad285c39caf61424493b1aac04be
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-05-08 20:53:11 +0200

    feat: add nlu service

--- Comparator/Services/WatsonService.cs
+++ Comparator/Services/WatsonService.cs
@@ -1,5 +1,38 @@
+using Comparator.Utils.Logger;
+using Comparator.Utils.Monads;
+using IBM.Cloud.SDK.Core.Authentication.Iam;
+using IBM.Watson.NaturalLanguageUnderstanding.v1;
+using IBM.Watson.NaturalLanguageUnderstanding.v1.Model;
+using Microsoft.AspNetCore.Http;
+
 namespace Comparator.Services {
     public class WatsonService : IWatsonService {
-        
+        private readonly ILoggerManager _logger;
+        private readonly NaturalLanguageUnderstandingService _nluService;
+
+        public WatsonService(ILoggerManager logger) {
+            //TODO: fetch apikey and url from config file
+            _logger = logger;
+            var authenticator = new IamAuthenticator(apikey: "");
+            _nluService = new NaturalLanguageUnderstandingService("1.0", authenticator);
+            _nluService.SetServiceUrl(
+                "");
+        }
+
+        /// <summary>
+        /// Performs a natural language understanding analysis using IBM Watson's natural
+        /// language understanding service
+        /// </summary>
+        /// <param name="features">Feature object containing information about the analysis to be performed</param>
+        /// <returns>Returns a capsule containing an AnalysisResults object</returns>
+        public Capsule<AnalysisResults> PerformNluAnalysis(Features features) {
+            var result = _nluService.Analyze(features);
+
+            if (result.StatusCode != StatusCodes.Status200OK) {
+                return new Failure<AnalysisResults>($"Status code: {result.StatusCode} Request to Watson failed", _logger);
+            }
+
+            return new Success<AnalysisResults>(result.Result);
+        }
     }
 }
