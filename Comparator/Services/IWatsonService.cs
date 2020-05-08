dda1cd32ff2f41fc43c469a90bd3a3fbf637c912
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-05-08 22:32:51 +0200

    refactor: add method to interface

--- Comparator/Services/IWatsonService.cs
+++ Comparator/Services/IWatsonService.cs
@@ -1,5 +1,8 @@
+using Comparator.Utils.Monads;
+using IBM.Watson.NaturalLanguageUnderstanding.v1.Model;
+
 namespace Comparator.Services {
     public interface IWatsonService {
-        
+        Capsule<AnalysisResults> PerformNluAnalysis(Features features);
     }
 }
