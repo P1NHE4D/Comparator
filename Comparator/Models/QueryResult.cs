f7dd89c82e1400a2b7632a0acf023c08d3b75b5e
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-05-10 15:31:14 +0200

    refactor: remove computation time from QueryResult

--- Comparator/Models/QueryResult.cs
+++ Comparator/Models/QueryResult.cs
@@ -1,10 +1,12 @@
 using System;
+using IBM.Watson.NaturalLanguageUnderstanding.v1.Model;
 
 namespace Comparator.Models {
     public class QueryResult {
         // TODO: Specify content
         public Query Query { get; set; }
         public int ProcessedDataSets { get; set; }
-        public double ComputationTime { get; set; }
+        public AnalysisResults Results { get; set; }
+        public string Message { get; set; }
     }
 }
