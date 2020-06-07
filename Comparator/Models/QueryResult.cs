eee647425ce26275da63563f37e52578b7541a39
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-06-07 15:16:25 +0200

    refactor: specify QueryResult model

--- Comparator/Models/QueryResult.cs
+++ Comparator/Models/QueryResult.cs
@@ -1,11 +1,14 @@
+using System;
+using System.Collections.Generic;
 using IBM.Watson.NaturalLanguageUnderstanding.v1.Model;
 
 namespace Comparator.Models {
     public class QueryResult {
-        // TODO: Specify content
-        public Query Query { get; set; }
         public int ProcessedDataSets { get; set; }
         public AnalysisResults Results { get; set; }
+        public double ObjATendency { get; set; }
+        public double ObjBTendency { get; set; }
+        public Dictionary<string, ClassifiedData> TermResults { get; set; }
         public string Message { get; set; }
     }
 }
