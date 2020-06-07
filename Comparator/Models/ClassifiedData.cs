884fb10e06dc68f082fb5da2586be53f7c37f259
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-06-07 15:15:32 +0200

    refactor: specify ClassifiedData model

--- Comparator/Models/ClassifiedData.cs
+++ Comparator/Models/ClassifiedData.cs
@@ -4,5 +4,8 @@ namespace Comparator.Models {
     public class ClassifiedData {
         public ICollection<string> ObjAData { get; set; }
         public ICollection<string> ObjBData { get; set; }
+        public double ObjATendency { get; set; }
+        public double ObjBTendency { get; set; }
+        public int DataCount { get; set; }
     }
 }
