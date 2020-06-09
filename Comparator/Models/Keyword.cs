2666be07b9a8ee6dfbd79656fce1da1dfde052d3
P1NHE4D <8gerlach@informatik.uni-hamburg.de>
2020-06-09 21:00:00 +0200

    refactor: add Keyword model

---
+++ Comparator/Models/Keyword.cs
@@ -0,0 +1,7 @@
+namespace Comparator.Models {
+    public class Keyword {
+        public string Text { get; set; }
+        public double? Relevance { get; set; }
+        public double? Sentiment { get; set; }
+    }
+}
